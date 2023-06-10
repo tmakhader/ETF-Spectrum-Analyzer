30dfb3e345a70d6fa729ee3f147c019d0570f1e2
TM Abdul Khader <54957433+tmakhader@users.noreply.github.com>
2023-06-09 21:01:59 -0400

    Create InstallDependency.sh

---
+++ InstallDependency.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Do chmod +x InstallDependency.sh to give execute rights to this script.
+# After giving execute permissions, run: source InstallDependency.sh
+# Install required dependencies using pip3
+pip3 install tkinter pandas numpy matplotlib yfinance
+
+# Exit the script with a success status
+exit 0
